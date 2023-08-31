05bfbf7bae845297d94eb8365480c76ff1d238ba
macanthonychukwuemeka <macanthonygreat@gmail.com>
2023-08-31 23:30:55 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import Form from "./Form"
+import { nanoid } from "nanoid"
+
+const App = () => {
+  const [items, setItems] = useState([])
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    }
+    //array of item will be added to our list from the newItem func
+    setItems([...items, newItem])
+  }
+  return (
+    <section className='section-center'>
+      <Form addItem={addItem} />
+    </section>
+  )
+}
+
+export default App
